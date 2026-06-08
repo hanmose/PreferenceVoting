8d0c87c7bbd6ea09da0bcc4079925a237f977886
hanmose <nightsound5124@gmail.com>
2026-06-08 13:18:34 +0900

    .

--- app/src/main/java/kr/ac/kopo/preferencevoting/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/preferencevoting/MainActivity.java
@@ -1,6 +1,11 @@
 package kr.ac.kopo.preferencevoting;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,46 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setTitle("아이돌 선호도 투표");
+
+//        투표수를 저장할 길이 9개 배열객체생성하고 0으로 초기화
+        final int[] voteCount = new int[9];
+        for (int i = 0; i < voteCount.length; i++) {
+            voteCount[i] = 0;
+        }
+
+//        ImageView객체의 참조값을 저장할 길이 9개 배열객체생성
+        ImageView[] imgvArr = new ImageView[9];
+        int[] imgvIdArr = {R.id.imgv1, R.id.imgv2, R.id.imgv3, R.id.imgv4, R.id.imgv5, R.id.imgv6, R.id.imgv7, R.id.imgv8, R.id.imgv9};
+
+        final String[] idolNameArr = {"블랙핑크", "아이브", "방탄소년단", "에스파", "아일릿", "뉴진스", "르세라핌", "스트레이키즈", "세븐틴"};
+
+        for (int i = 0; i < imgvArr.length; i++) {
+            final int index;
+            index = i;
+            imgvArr[index] = findViewById(imgvIdArr[index]);
+            imgvArr[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    voteCount[index]++;
+                    Toast.makeText(getApplicationContext(), idolNameArr[index] + ", 총 " + voteCount[index] + " 표", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        Button btnFinish = findViewById(R.id.btn_finish);
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //        ResultActivity를 시작하는 Intent객체를 생성하고 값을 넣는다.
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                intent.putExtra("voteCount", voteCount);
+                intent.putExtra("idolNameArr", idolNameArr);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/kr/ac/kopo/preferencevoting/ResuitActivity.java
+++
@@ -1,4 +0,0 @@
-package kr.ac.kopo.preferencevoting;
-
-public class ResuitActivity {
-}
